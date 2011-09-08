cd2810599b4cf99883db0441e89518a63a034a9f
Raj Kiran <rajkiran.b@indigenius.com>
2011-09-08 15:25:57 +0530

    rails application is added in the second commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testproj::Application.config.secret_token = '5843efef63dd9824161cd3fbb8b721cc12fdb130ca8b6bc836b1d99c9ed776f92853b5ec6577cf53a54a811856d84b49540ff71ca436220ac83018500d11c908'
